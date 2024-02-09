d3cec6992daba783ffa330f2b72b29139fc1f7f3
yuvalerdreich <erdreich.yuval@post.runi.ac.il>
2024-02-09 21:58:05 +0200

    HW8

--- PlayList.java
+++ PlayList.java
@@ -118,7 +118,7 @@ class PlayList {
      *  If the list is empty, or the given index is negative or too big for this list, 
      *  does nothing and returns -1. */
     public void remove(int i) {
-        if (this.size != 0 || i > 0 && i <= this.size) {
+        if (this.size != 0 && i > 0 && i <= this.size) {
             if (i == this.size - 1) {
                 this.tracks[i] = null;
             } else {
